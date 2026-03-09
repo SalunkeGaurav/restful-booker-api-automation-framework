82572606edf4d5b4a6dde720b581cbe3c8c41c8c
GauravSalunke <gaurav.salunke.work@gmail.com>
2026-03-09 10:45:00 +0530

    add BOOKING_SCHEMA constant in FrameworkConstants

--- src/main/java/com/restfulbooker/utils/FrameworkConstants.java
+++ src/main/java/com/restfulbooker/utils/FrameworkConstants.java
@@ -10,4 +10,6 @@ public final class FrameworkConstants {
     public static final String ACCEPT = "Accept";
     public static final String APPLICATION_JSON = "application/json";
     public static final String BOOKINGS_JSON_PATH = "src/test/resources/testdata/booking.json";
+    public static final String BOOKING_SCHEMA = "schemas/booking-schema.json";
+
 }
